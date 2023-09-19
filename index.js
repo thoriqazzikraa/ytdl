58dcd028f81514cb364bcfd0b3f58a8720c1a648
thoriqazzikraa <thoriqazzikra17@gmail.com>
2023-09-19 16:14:39 +0700

    up

--- index.js
+++ index.js
@@ -1,422 +1,119 @@
 const axios = require("axios");
-axios.defaults.headers.post["User-Agent"] = "WhatsApp/2.5.3"
 const convertSec = require("./function");
 const fetch = require("node-fetch");
 
 async function yt(url) {
-  function _0x49ad() {
-    const _0x483d4a = [
-      "https://ww",
-      "6hOsmED",
-      "applicatio",
-      "5IwLqaF",
-      "184047NrkcjO",
-      "PostmanRun",
-      "5449843zrtHUG",
-      "time/7.32.",
-      "372NrjCVT",
-      "1569465coOhMk",
-      "n/x-www-fo",
-      "post",
-      "rm-urlenco",
-      "2990802GcZxOH",
-      "28787DRJtXb",
-      "30ZNUoob",
-      "w.y2mate.c",
-      "99scvJad",
-      "3721680nnBaMp",
-      "jax",
-      "67604VZIMBE",
-      "om/mates/a",
-      "home",
-      "nalyzeV2/a",
-      "ded",
-    ];
-    _0x49ad = function () {
-      return _0x483d4a;
-    };
-    return _0x49ad();
-  }
-  const _0x1a56f2 = _0x5143;
-  (function (_0x138abb, _0x5945a4) {
-    const _0x3bc494 = _0x5143,
-      _0x13ffa1 = _0x138abb();
-    while (!![]) {
-      try {
-        const _0x11cd48 =
-          (parseInt(_0x3bc494(0x82)) / (0x151 * 0xb + 0x24e3 + -0x335d)) *
-            (-parseInt(_0x3bc494(0x7f)) / (0xb23 + -0x1e92 + 0x1371)) +
-          (-parseInt(_0x3bc494(0x8f)) / (0x13e9 + -0x1efc * 0x1 + 0xb16)) *
-            (-parseInt(_0x3bc494(0x92)) / (0x19e5 + 0x2c7 * 0x9 + -0x32e0)) +
-          (parseInt(_0x3bc494(0x81)) / (0x1113 + -0x73d + -0x9d1)) *
-            (-parseInt(_0x3bc494(0x8b)) /
-              (-0x2 * 0x1034 + -0xa29 + 0x1 * 0x2a97)) +
-          parseInt(_0x3bc494(0x84)) / (-0x234f + 0x4b * -0x83 + -0x71 * -0xa7) +
-          -parseInt(_0x3bc494(0x90)) / (-0x712 + -0x1625 + 0x1d3f) +
-          (parseInt(_0x3bc494(0x87)) / (0x241e + -0x3ae + -0x7 * 0x4a1)) *
-            (parseInt(_0x3bc494(0x8d)) / (0x611 + -0x249f + 0x1e98)) +
-          (parseInt(_0x3bc494(0x8c)) / (0x3af * 0x9 + -0x1617 + -0x1 * 0xb05)) *
-            (parseInt(_0x3bc494(0x86)) /
-              (0x2 * 0x823 + 0x1b5d + -0x2b97 * 0x1));
-        if (_0x11cd48 === _0x5945a4) break;
-        else _0x13ffa1["push"](_0x13ffa1["shift"]());
-      } catch (_0x15f71d) {
-        _0x13ffa1["push"](_0x13ffa1["shift"]());
-      }
-    }
-  })(_0x49ad, 0x1fedd + 0x9038a + -0x4873d);
-  const { data } = await axios(
-    _0x1a56f2(0x7e) +
-      _0x1a56f2(0x8e) +
-      _0x1a56f2(0x93) +
-      _0x1a56f2(0x95) +
-      _0x1a56f2(0x91),
-    {
-      method: _0x1a56f2(0x89),
-      data: { k_query: url, k_page: _0x1a56f2(0x94), hl: "id", q_auto: 0x0 },
-      headers: {
-        "content-type":
-          _0x1a56f2(0x80) + _0x1a56f2(0x88) + _0x1a56f2(0x8a) + _0x1a56f2(0x96),
-        "user-agent": _0x1a56f2(0x83) + _0x1a56f2(0x85) + "2",
-      },
-    }
-  );
-  function _0x5143(_0x572da8, _0xf8a224) {
-    const _0x226adf = _0x49ad();
-    return (
-      (_0x5143 = function (_0x10b2e4, _0x4efdf5) {
-        _0x10b2e4 = _0x10b2e4 - (0x20e * -0x2 + -0xd38 * -0x2 + -0x56 * 0x41);
-        let _0x13b61a = _0x226adf[_0x10b2e4];
-        return _0x13b61a;
-      }),
-      _0x5143(_0x572da8, _0xf8a224)
-    );
-  }
+  const { data } = await axios(`https://www.y2mate.com/mates/analyzeV2/ajax`, {
+    method: "post",
+    data: { k_query: url, k_page: "home", hl: "id", q_auto: 0 },
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "user-agent": "WhatsApp/2.5.3",
+    },
+  });
   return data;
 }
 
 async function ytmp3(url) {
-  function _0x4d70() {
-    const _0x43b3c3 = [
-      "11ENzeVC",
-      "Can\x27t\x20get\x20",
-      "735870SphEKq",
-      "Kbps",
-      "PostmanRun",
-      "568AACJUy",
-      "ftype",
-      "ytimg.com/",
-      "metadata",
-      "fquality",
-      "ault.jpg",
-      ".jpg",
-      "6HaFQpV",
-      "title",
-      "om/mates/c",
-      "152eETBTI",
-      "266688iXQleC",
-      "https://i.",
-      "rm-urlenco",
-      "mp3",
-      "log",
-      "onvertV2/i",
-      "1231083adIvPU",
-      "dlink",
-      "https://ww",
-      "646540riWxnx",
-      "n/x-www-fo",
-      "w.y2mate.c",
-      "/hqdefault",
-      "15VsyJEg",
-      "links",
-      "ded",
-      "/maxresdef",
-      "297560nXbvFv",
-      "vid",
-      "mp3128",
-      "272237WtsoBW",
-      "post",
-      "time/7.32.",
-      "ndex",
-      "vi/",
-      "applicatio",
-      "size",
-    ];
-    _0x4d70 = function () {
-      return _0x43b3c3;
-    };
-    return _0x4d70();
-  }
-  const _0x209543 = _0x4911;
-  function _0x4911(_0x2dd20f, _0x3aef50) {
-    const _0x89d078 = _0x4d70();
-    return (
-      (_0x4911 = function (_0x5e675c, _0x18be8e) {
-        _0x5e675c = _0x5e675c - (0x1fd * -0x3 + -0x334 + 0x9b0);
-        let _0x1550dc = _0x89d078[_0x5e675c];
-        return _0x1550dc;
-      }),
-      _0x4911(_0x2dd20f, _0x3aef50)
-    );
-  }
-  (function (_0x1ed07e, _0x2147c6) {
-    const _0xaf2269 = _0x4911,
-      _0xf331e8 = _0x1ed07e();
-    while (!![]) {
-      try {
-        const _0x5ae3b2 =
-          (-parseInt(_0xaf2269(0x86)) /
-            (-0x1 * -0x1c53 + -0x8cb * -0x2 + -0x2de8)) *
-            (parseInt(_0xaf2269(0xa7)) /
-              (-0x385 * -0x5 + 0xa * -0x14f + -0x481)) +
-          -parseInt(_0xaf2269(0xa4)) / (0x1613 + -0x2300 + 0xcf0) +
-          (-parseInt(_0xaf2269(0x98)) /
-            (-0x1 * 0x1841 + -0x1d4b * -0x1 + 0x283 * -0x2)) *
-            (-parseInt(_0xaf2269(0x94)) / (0x1f72 + 0x229d + -0x420a)) +
-          (-parseInt(_0xaf2269(0xae)) /
-            (0x1 * -0xbb7 + -0x248e + -0xd * -0x3b7)) *
-            (-parseInt(_0xaf2269(0x9b)) / (-0x16b7 + -0xede + 0x259c)) +
-          -parseInt(_0xaf2269(0x87)) / (-0x1872 + 0x22ee + -0xa74) +
-          parseInt(_0xaf2269(0x8d)) / (-0x1ec9 + 0x2174 + -0x2a2) +
-          (parseInt(_0xaf2269(0x90)) /
-            (0x15d * -0x1a + -0x1 * -0x106a + -0x1 * -0x1312)) *
-            (parseInt(_0xaf2269(0xa2)) / (0x1 * -0xea7 + -0x17af + 0x2661));
-        if (_0x5ae3b2 === _0x2147c6) break;
-        else _0xf331e8["push"](_0xf331e8["shift"]());
-      } catch (_0x106751) {
-        _0xf331e8["push"](_0xf331e8["shift"]());
-      }
-    }
-  })(_0x4d70, -0x225f6 + -0x2e2a1 + 0x73223);
   try {
-    const info = await yt(url),
-      { data } = await axios(
-        _0x209543(0x8f) +
-          _0x209543(0x92) +
-          _0x209543(0x85) +
-          _0x209543(0x8c) +
-          _0x209543(0x9e),
-        {
-          method: _0x209543(0x9c),
-          data: {
-            vid: info[_0x209543(0x99)],
-            k: info[_0x209543(0x95)][_0x209543(0x8a)][_0x209543(0x9a)]["k"],
-          },
-          headers: {
-            "content-type":
-              _0x209543(0xa0) +
-              _0x209543(0x91) +
-              _0x209543(0x89) +
-              _0x209543(0x96),
-            "user-agent": _0x209543(0xa6) + _0x209543(0x9d) + "2",
-          },
-        }
-      ),
-      thumb =
-        _0x209543(0x88) +
-        _0x209543(0xa9) +
-        _0x209543(0x9f) +
-        info[_0x209543(0x99)] +
-        (_0x209543(0x97) + _0x209543(0xac)),
-      { status } = await fetch(thumb);
-    if (status !== 0x16c8 + -0x1 * -0x156a + -0x2b6a)
-      var thumbImg =
-        _0x209543(0x88) +
-        _0x209543(0xa9) +
-        _0x209543(0x9f) +
-        info[_0x209543(0x99)] +
-        (_0x209543(0x93) + _0x209543(0xad));
-    else var thumbImg = thumb;
+    const info = await yt(url);
+    const { data } = await axios(
+      `https://www.y2mate.com/mates/convertV2/index`,
+      {
+        method: "post",
+        data: {
+          vid: info.vid,
+          k: info.links.mp3.mp3128.k,
+        },
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "user-agent": "WhatsApp/2.5.3",
+        },
+      }
+    );
+    const thumb = `https://i.ytimg.com/vi/${info.vid}/maxresdefault.jpg`;
+    const { status } = await fetch(thumb);
+    if (status !== 200) {
+      var thumbImg = `https://i.ytimg.com/vi/${info.vid}/hqdefault.jpg`;
+    } else {
+      var thumbImg = thumb;
+    }
     const result = {
-      status: !![],
-      title: data[_0x209543(0xaf)],
-      channel: info["a"],
-      duration: convertSec(info["t"]),
-      size: info[_0x209543(0x95)][_0x209543(0x8a)][_0x209543(0x9a)][
-        _0x209543(0xa1)
-      ],
-      type: data[_0x209543(0xa8)],
-      quality: data[_0x209543(0xab)] + _0x209543(0xa5),
-      id: info[_0x209543(0x99)],
+      status: true,
+      title: data.title,
+      channel: info.a,
+      duration: convertSec(info.t),
+      size: info.links.mp3.mp3128.size,
+      type: data.ftype,
+      quality: `${data.fquality}Kbps`,
+      id: info.vid,
       thumbnail: thumbImg,
-      url: data[_0x209543(0x8e)],
+      url: data.dlink,
     };
     return result;
   } catch {
-    const result = { status: ![], message: _0x209543(0xa3) + _0x209543(0xaa) };
-    return console[_0x209543(0x8b)](result), result;
+    const result = {
+      status: false,
+      message: "Can't get metadata",
+    };
+    console.log(result);
+    return result;
   }
 }
 
 async function ytmp4(url, quality) {
-  var _0x32809f = _0x1a51;
-  (function (_0x4370d1, _0x35ad42) {
-    var _0x5362bb = _0x1a51,
-      _0xf033f8 = _0x4370d1();
-    while (!![]) {
-      try {
-        var _0x39010b =
-          (parseInt(_0x5362bb(0x1fe)) / (0xd6 + 0xde3 + -0xeb8)) *
-            (parseInt(_0x5362bb(0x1f1)) / (-0x1c50 + -0x147d + -0xf * -0x341)) +
-          parseInt(_0x5362bb(0x1fa)) /
-            (-0x2 * -0x121a + -0x8d * 0xa + -0x1eaf) +
-          -parseInt(_0x5362bb(0x20c)) /
-            (-0x6 * -0x52b + 0x136b * 0x1 + -0x3269) +
-          (-parseInt(_0x5362bb(0x1f2)) / (0x89 * -0x1d + 0x235b + -0x13d1)) *
-            (-parseInt(_0x5362bb(0x1f5)) /
-              (-0x1 * 0x1061 + -0x244 * 0x11 + 0x36eb)) +
-          parseInt(_0x5362bb(0x207)) / (-0x340 + 0x177a + -0x1433) +
-          (parseInt(_0x5362bb(0x212)) / (0x229e + -0x1940 + 0xa * -0xef)) *
-            (parseInt(_0x5362bb(0x20b)) / (-0x29 * 0xa5 + -0x62c + 0x20a2)) +
-          (parseInt(_0x5362bb(0x1ff)) /
-            (0x2 * 0x50f + -0x2641 + -0x1c2d * -0x1)) *
-            (-parseInt(_0x5362bb(0x205)) /
-              (0xf57 + 0xf7 * 0x1b + 0x49 * -0x91));
-        if (_0x39010b === _0x35ad42) break;
-        else _0xf033f8["push"](_0xf033f8["shift"]());
-      } catch (_0x61c8bb) {
-        _0xf033f8["push"](_0xf033f8["shift"]());
-      }
-    }
-  })(_0x44f8, -0x1152e3 + -0xd5c5c * -0x2 + 0x52961);
-  function _0x1a51(_0x13eda3, _0x53e908) {
-    var _0x90b629 = _0x44f8();
-    return (
-      (_0x1a51 = function (_0x4278cc, _0x3806ba) {
-        _0x4278cc = _0x4278cc - (-0x8 * -0x33d + -0x132d + -0x1 * 0x4ca);
-        var _0x23581f = _0x90b629[_0x4278cc];
-        return _0x23581f;
-      }),
-      _0x1a51(_0x13eda3, _0x53e908)
-    );
-  }
   try {
     const info = await yt(url);
-    if (quality === -0xe39 + -0x1287 + 0x24f8 * 0x1)
-      var res = -0x156f + 0x213c + -0xb44;
-    else {
-      if (quality === 0x40e + -0xba6 + -0x18 * -0x6f)
-        var res = 0x37 * -0x36 + -0x1 * -0xb17 + 0x99;
-      else {
-        if (quality === -0x185 * 0x10 + 0x24b + -0x1 * -0x17e5)
-          var res = -0x1284 * 0x1 + -0x1b * -0x59 + 0x9a8;
-        else {
-          if (
-            quality === -0x2173 + -0x806 + 0x2ae1 ||
-            quality === undefined ||
-            quality === -0xb87 + -0x10f * 0x2 + 0xda5
-          )
-            var res = -0x17db * 0x1 + -0x1 * 0x25d9 + 0x3dc6;
-        }
-      }
+    if (quality === 1080) {
+      var res = 137;
+    } else if (quality === 720) {
+      var res = 22;
+    } else if (quality === 480) {
+      var res = 135;
+    } else if (quality === 360 || quality === undefined || quality === 0) {
+      var res = 18;
     }
     const { data } = await axios(
-        _0x32809f(0x219) +
-          _0x32809f(0x209) +
-          _0x32809f(0x1fc) +
-          _0x32809f(0x1fd) +
-          _0x32809f(0x204),
-        {
-          method: _0x32809f(0x1f4),
-          data: {
-            vid: info[_0x32809f(0x1f8)],
-            k: info[_0x32809f(0x208)][_0x32809f(0x20e)]["" + res]["k"],
-          },
-          headers: {
-            "content-type":
-              _0x32809f(0x200) +
-              _0x32809f(0x20a) +
-              _0x32809f(0x1f6) +
-              _0x32809f(0x20f),
-            "user-agent": _0x32809f(0x203) + _0x32809f(0x1f9) + "2",
-          },
-        }
-      ),
-      thumb =
-        _0x32809f(0x20d) +
-        _0x32809f(0x213) +
-        _0x32809f(0x1f3) +
-        info[_0x32809f(0x1f8)] +
-        (_0x32809f(0x215) + _0x32809f(0x211)),
-      { status } = await fetch(thumb);
-    if (status !== -0x2 * -0xd46 + 0x2 * -0x773 + -0xd * 0xd6)
-      var thumbImg =
-        _0x32809f(0x20d) +
-        _0x32809f(0x213) +
-        _0x32809f(0x1f3) +
-        info[_0x32809f(0x1f8)] +
-        (_0x32809f(0x217) + _0x32809f(0x1fb));
-    else var thumbImg = thumb;
+      `https://www.y2mate.com/mates/convertV2/index`,
+      {
+        method: "post",
+        data: {
+          vid: info.vid,
+          k: info.links.mp4[`${res}`].k,
+        },
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "user-agent": "WhatsApp/2.5.3",
+        },
+      }
+    );
+    const thumb = `https://i.ytimg.com/vi/${info.vid}/maxresdefault.jpg`;
+    const { status } = await fetch(thumb);
+    if (status !== 200) {
+      var thumbImg = `https://i.ytimg.com/vi/${info.vid}/hqdefault.jpg`;
+    } else {
+      var thumbImg = thumb;
+    }
     const result = {
-      status: !![],
-      title: data[_0x32809f(0x1f7)],
-      channel: info["a"],
-      duration: convertSec(info["t"]),
-      size: info[_0x32809f(0x208)][_0x32809f(0x20e)]["" + res][
-        _0x32809f(0x202)
-      ],
-      type: data[_0x32809f(0x210)],
-      quality: data[_0x32809f(0x216)],
-      id: info[_0x32809f(0x1f8)],
+      status: true,
+      title: data.title,
+      channel: info.a,
+      duration: convertSec(info.t),
+      size: info.links.mp4[`${res}`].size,
+      type: data.ftype,
+      quality: data.fquality,
+      id: info.vid,
       thumbnail: thumbImg,
-      url: decodeURIComponent(data[_0x32809f(0x201)]),
+      url: decodeURIComponent(data.dlink),
     };
     return result;
   } catch {
     const result = {
-      status: ![],
-      message: _0x32809f(0x206) + _0x32809f(0x218),
+      status: false,
+      message: "Can't get metadata",
     };
-    return console[_0x32809f(0x214)](result), result;
-  }
-  function _0x44f8() {
-    var _0x28e3f2 = [
-      "ault.jpg",
-      "192PtoEOC",
-      "ytimg.com/",
-      "log",
-      "/maxresdef",
-      "fquality",
-      "/hqdefault",
-      "metadata",
-      "https://ww",
-      "41074EsLPOc",
-      "75Tvbidf",
-      "vi/",
-      "post",
-      "418308cSMlTw",
-      "rm-urlenco",
-      "title",
-      "vid",
-      "time/7.32.",
-      "3909396iEBQzQ",
-      ".jpg",
-      "om/mates/c",
-      "onvertV2/i",
-      "71EkBSBS",
-      "310tFBsxk",
-      "applicatio",
-      "dlink",
-      "size",
-      "PostmanRun",
-      "ndex",
-      "1159543QuVfMH",
-      "Can\x27t\x20get\x20",
-      "8115597wcFeUm",
-      "links",
-      "w.y2mate.c",
-      "n/x-www-fo",
-      "181557vwtsaD",
-      "4914332CBQaAS",
-      "https://i.",
-      "mp4",
-      "ded",
-      "ftype",
-    ];
-    _0x44f8 = function () {
-      return _0x28e3f2;
-    };
-    return _0x44f8();
+    console.log(result);
+    return result;
   }
 }
 
