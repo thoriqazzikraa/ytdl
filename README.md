a29eaf7c06ea54b4adffe79af957d6046f6e7d32
thoriqazzikraa <thoriqazzikra17@gmail.com>
2023-09-23 18:04:21 +0700

    fixed 1080p

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
 # Installation
-`npm i @nechlophomeriaa/ytdl`
+
+```sh
+> npm i @nechlophomeriaa/ytdl
+```
 
 # Usage
-`const { ytmp3, ytmp4 } = require("@nechlophomeriaa/ytdl")`
+
+```js
+const { ytmp3, ytmp4 } = require("@nechlophomeriaa/ytdl")
+```
 
 # Example Audio Downloader
+
 ```js
-(async () => {
-const yt = await ytmp3("https://youtu.be/R95ILhksGt8")
-console.log(yt)
-})
+;async () => {
+  const yt = await ytmp3("https://youtu.be/R95ILhksGt8")
+  console.log(yt)
+}
 ```
 
 # Example Video Downloader
+
 ```js
-(async () => {
-const yt = await ytmp4("https://youtu.be/R95ILhksGt8", 1080) //If quality is undefined, it will resolve with 360p quality
-console.log(yt)
-})
+;async () => {
+  const yt = await ytmp4("https://youtu.be/R95ILhksGt8", 1080) //If quality is undefined, it will resolve with 360p quality
+  console.log(yt)
+}
 ```
 
-Thanks for using this module
+<p>Thanks for using this module</p>
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const axios = require("axios");
-const convertSec = require("./function");
-const fetch = require("node-fetch");
+const axios = require("axios")
+const convertSec = require("./function")
+const fetch = require("node-fetch")
 
 async function yt(url) {
   const { data } = await axios(`https://www.y2mate.com/mates/analyzeV2/ajax`, {
@@ -8,35 +8,32 @@ async function yt(url) {
     data: { k_query: url, k_page: "home", hl: "id", q_auto: 0 },
     headers: {
       "content-type": "application/x-www-form-urlencoded",
-      "user-agent": "WhatsApp/2.5.3",
-    },
-  });
-  return data;
+      "user-agent": "WhatsApp/2.5.3"
+    }
+  })
+  return data
 }
 
 async function ytmp3(url) {
   try {
-    const info = await yt(url);
-    const { data } = await axios(
-      `https://www.y2mate.com/mates/convertV2/index`,
-      {
-        method: "post",
-        data: {
-          vid: info.vid,
-          k: info.links.mp3.mp3128.k,
-        },
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          "user-agent": "WhatsApp/2.5.3",
-        },
+    const info = await yt(url)
+    const { data } = await axios(`https://www.y2mate.com/mates/convertV2/index`, {
+      method: "post",
+      data: {
+        vid: info.vid,
+        k: info.links.mp3.mp3128.k
+      },
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "user-agent": "WhatsApp/2.5.3"
       }
-    );
-    const thumb = `https://i.ytimg.com/vi/${info.vid}/maxresdefault.jpg`;
-    const { status } = await fetch(thumb);
+    })
+    const thumb = `https://i.ytimg.com/vi/${info.vid}/maxresdefault.jpg`
+    const { status } = await fetch(thumb)
     if (status !== 200) {
-      var thumbImg = `https://i.ytimg.com/vi/${info.vid}/hqdefault.jpg`;
+      var thumbImg = `https://i.ytimg.com/vi/${info.vid}/hqdefault.jpg`
     } else {
-      var thumbImg = thumb;
+      var thumbImg = thumb
     }
     const result = {
       status: true,
@@ -48,51 +45,53 @@ async function ytmp3(url) {
       quality: `${data.fquality}Kbps`,
       id: info.vid,
       thumbnail: thumbImg,
-      url: data.dlink,
-    };
-    return result;
+      url: data.dlink
+    }
+    return result
   } catch {
     const result = {
       status: false,
-      message: "Can't get metadata",
-    };
-    console.log(result);
-    return result;
+      message: "Can't get metadata"
+    }
+    console.log(result)
+    return result
   }
 }
 
 async function ytmp4(url, quality) {
   try {
-    const info = await yt(url);
-    if (quality === 1080) {
-      var res = 137;
-    } else if (quality === 720) {
-      var res = 22;
-    } else if (quality === 480) {
-      var res = 135;
-    } else if (quality === 360 || quality === undefined || quality === 0) {
-      var res = 18;
-    }
-    const { data } = await axios(
-      `https://www.y2mate.com/mates/convertV2/index`,
-      {
-        method: "post",
-        data: {
-          vid: info.vid,
-          k: info.links.mp4[`${res}`].k,
-        },
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          "user-agent": "WhatsApp/2.5.3",
-        },
-      }
-    );
-    const thumb = `https://i.ytimg.com/vi/${info.vid}/maxresdefault.jpg`;
-    const { status } = await fetch(thumb);
-    if (status !== 200) {
-      var thumbImg = `https://i.ytimg.com/vi/${info.vid}/hqdefault.jpg`;
+    const info = await yt(url)
+    if (!info.links.mp4["137"]) {
+      var qualityHD = 299
     } else {
-      var thumbImg = thumb;
+      var qualityHD = 137
+    }
+    if (quality === 1080) {
+      var res = qualityHD
+    } else if (quality === 720) {
+      var res = 22
+    } else if (quality === 480) {
+      var res = 135
+    } else if (quality === 360 || quality === undefined || quality === 0) {
+      var res = 18
+    }
+    const { data } = await axios(`https://www.y2mate.com/mates/convertV2/index`, {
+      method: "post",
+      data: {
+        vid: info.vid,
+        k: info.links.mp4[`${res}`].k
+      },
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "user-agent": "WhatsApp/2.5.3"
+      }
+    })
+    const thumb = `https://i.ytimg.com/vi/${info.vid}/maxresdefault.jpg`
+    const { status } = await fetch(thumb)
+    if (status !== 200) {
+      var thumbImg = `https://i.ytimg.com/vi/${info.vid}/hqdefault.jpg`
+    } else {
+      var thumbImg = thumb
     }
     const result = {
       status: true,
@@ -104,17 +103,17 @@ async function ytmp4(url, quality) {
       quality: data.fquality,
       id: info.vid,
       thumbnail: thumbImg,
-      url: decodeURIComponent(data.dlink),
-    };
-    return result;
+      url: decodeURIComponent(data.dlink)
+    }
+    return result
   } catch {
     const result = {
       status: false,
-      message: "Can't get metadata",
-    };
-    console.log(result);
-    return result;
+      message: "Can't get metadata"
+    }
+    console.log(result)
+    return result
   }
 }
 
-module.exports = { yt, ytmp3, ytmp4 };
+module.exports = { yt, ytmp3, ytmp4 }
